3de656a82b0df16ab043ea732d0b2a6d1a23e31e
Carlos Machado <machadocarlosdev@outlook.com>
2025-07-28 20:43:11 -0300

    Sistema para realizar cálculo de frete rápido

---
+++ CalculoFreteRapido.java
@@ -0,0 +1,25 @@
+package CalculoFrete;
+import java.util.Scanner;
+
+public class CalculoFreteRapido {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //TODO: Leia o peso em quilos:
+        System.out.println("Informe o peso da carga(kg): ");
+        double pesoCarga = scanner.nextDouble();
+
+        // TODO: Leia o valor do frete por quilo:
+        System.out.println("Informe o valor do frete por quilo: ");
+        double valorFreteQuilo = scanner.nextDouble();
+
+        //TODO: Calcule o valor total do frete:
+        double valorTotal = pesoCarga * valorFreteQuilo;
+
+        // TODO: Exiba o valor formatado com duas casas decimais:
+        System.out.printf("O valor total do frete é de R$%.2f", valorTotal);
+
+        scanner.close();
+    }
+
+}
