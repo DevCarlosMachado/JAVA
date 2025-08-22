0b1a41ca4a161afc8853d8aa68687939433d6a83
Carlos Machado <machadocarlosdev@outlook.com>
2025-08-22 15:25:00 -0300

    Adicionando comentários explicativos no código

--- EmployeeSalaryIncrease/src/application/Program.java
+++ EmployeeSalaryIncrease/src/application/Program.java
@@ -49,11 +49,11 @@ public class Program {
         System.out.println();
         System.out.print("Enter the employee id that will have salary increase: ");
         int idSalary = sc.nextInt();
-        Integer pos = position(list, idSalary);
+        Integer pos = position(list, idSalary); // Find the position of the employee with the given id
         if (pos == null) {
-            System.out.println("This id does not exist!");
+            System.out.println("This id does not exist!"); // If the position is null, it means the id was not found
         } else {
-            System.out.print("Enter the percentage: ");
+            System.out.print("Enter the percentage: "); // If the position is found, ask for the percentage to increase the salary
             double percent = sc.nextDouble();
             list.get(pos).increaseSalary(percent);
         }
@@ -65,17 +65,18 @@ public class Program {
         sc.close();
     }
 
+    // Method to find the position of an employee by id
     public static Integer position(List<Employee> list, int id) {
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).getId() == id) {
-                return i;
+                return i; // Return the index of the employee with the given id
             }
         }
-        return null;
+        return null; // Return null if no employee with the given id is found
     }
 
     public static boolean hasId(List<Employee> list, int id) {
-        Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+        Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null); //Verificador para não haver repetição de id
         return emp != null;
     }
 
